519c9a81be14b80ecc3fcb018de348a1cac9d1d5
luisdvalle <luisdvalle@users.noreply.github.com>
2020-07-06 22:20:10 +1000

    Adding Startup
    And registering services into IoC

---
+++ UsersApi/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using UsersApi.Config;
+using UsersApi.Services;
+using UsersApi.Storage;
+
+[assembly: FunctionsStartup(typeof(UsersApi.Startup))]
+
+namespace UsersApi
+{
+    public class Startup : FunctionsStartup
+    {
+        private readonly IConfigurationSettings _configurationSettings;
+
+        public Startup()
+        {
+            _configurationSettings = new ConfigurationSettings();
+        }
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<IRepository, Repository>(provider =>
+                new Repository(_configurationSettings.AzureWebJobsStorage));
+
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<IAccountService, AccountService>();
+        }
+    }
+}

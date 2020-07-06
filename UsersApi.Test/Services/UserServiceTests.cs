59b6fa6e7787ec38e1836e0cb224d5bc0b85e452
luisdvalle <luisdvalle@users.noreply.github.com>
2020-07-06 22:34:52 +1000

    Initial commit for Test project

---
+++ UsersApi.Test/Services/UserServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace UsersApi.Test.Services
+{
+    public class UserServiceTests
+    {
+        public UserServiceTests()
+        {
+            
+        }
+
+        [Fact]
+        public async Task CreateUserAsync_UserIsValid_ReturnsSuccessfulNoContentResponse()
+        {
+
+        }
+    }
+}
